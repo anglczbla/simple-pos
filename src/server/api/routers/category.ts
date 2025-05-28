784295f71f346e98a72cd2e96b781eb6493fb75c
angel <angelicaizabella90@gmail.com>
2025-05-28 21:12:49 +0700

    first commit

---
+++ src/server/api/routers/category.ts
@@ -0,0 +1,17 @@
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const categoryRouter = createTRPCRouter({
+    getCategories: protectedProcedure.query(async ({ ctx })) => {
+        const { db } = ctx;
+
+        const categories = await db.category.findMany();
+        select:{
+            id:true,
+            name:true
+        }
+
+        return categories;
+
+    }
+})
+    
